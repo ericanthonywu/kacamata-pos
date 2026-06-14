196b64279bbb0d02cb3b2bb27a33ae2208151a9d
ericanthony <ericanthony@ericanthony.com>
2026-06-14 19:15:24 +0700

    feat: restrict sales record deletion to admin and add delete button to UI

--- src/routes/penjualan.routes.js
+++ src/routes/penjualan.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const auth = require('../middleware/auth');
+const { requireAdmin } = require('../middleware/rbac');
 const c = require('../controllers/penjualan.controller');
 
 router.get('/', auth, c.index);
@@ -7,6 +8,6 @@ router.get('/dt', auth, c.datatables);
 router.get('/baru', auth, c.createForm);
 router.post('/', auth, c.store);
 router.get('/:id', auth, c.show);
-router.delete('/:id', auth, c.destroy);
+router.delete('/:id', auth, requireAdmin, c.destroy);
 
 module.exports = router;
